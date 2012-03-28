3e4fc5c7d744328c840503a207edf5e3b33ebf30
Connor McCoy <cmccoy@fhcrc.org>
2012-03-28 14:41:23 -0700

    A little test

---
+++ small/get_refs.sh
@@ -0,0 +1,2 @@
+../refset.py search-sequences dedup.fasta ../../../../micro_refset/corpus/build/rdp.fasta small.db --weights dedup_info.csv
+../refset.py select-references small.db ../../../../micro_refset/corpus/build/rdp.db test_refs.fasta  
