885617dd5a4b367f28a07792cc21c24194265416
Connor McCoy <cmccoy@fhcrc.org>
2012-03-29 12:00:45 -0700

    Update subcommand

--- deenurp/subcommands/search_sequences.py
+++ deenurp/subcommands/search_sequences.py
@@ -10,10 +10,12 @@ from .. import search
 
 def build_parser(p):
     p.add_argument('sequence_file', help="""Fasta file containing query
-            sequences""")
-    p.add_argument('sequence_database', help="""Sequence database for possible
-            homologs""")
-    p.add_argument('output', help="""Output database to write""")
+            sequences""", metavar='<query_fasta>')
+    p.add_argument('output', help="""Output database to write""", metavar='<output_db>')
+    p.add_argument('sequence_databases', metavar='<db_fasta>', help="""Sequence
+            database for possible reference sequences. Databases are searched
+            in order. Any clusters with hits to an earlier database are not
+            re-searched.""", nargs='+')
     p.add_argument('--weights', help="""Weights, in a `guppy dedup
             -m`-compatible dedup file""", type=argparse.FileType('r'))
     uc = p.add_argument_group('UCLUST')
@@ -34,7 +36,7 @@ def action(args):
         with args.weights:
             weights = search.dedup_info_to_counts(args.weights)
         assert weights
-    search.create_database(con, args.sequence_file, args.sequence_database,
+    search.create_database(con, args.sequence_file, args.sequence_databases,
             weights=weights, maxaccepts=args.maxaccepts,
             maxrejects=args.maxrejects, cluster_id=args.cluster_identity,
             search_id=args.search_identity, quiet=args.verbosity == 0)
