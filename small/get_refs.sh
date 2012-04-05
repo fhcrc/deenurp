235ffce96adeca655d699329ae36b915bc48cd2b
Connor McCoy <cmccoy@fhcrc.org>
2012-04-05 13:18:24 -0700

    Remove test case

--- small/get_refs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-set -u
-
-../refset.py search-sequences dedup.fasta small.db --weights dedup_info.csv ../../../../micro_refset/corpus/build/rdp.fasta
-../refset.py select-references small.db test_refs.fasta
