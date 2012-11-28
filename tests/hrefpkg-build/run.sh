f2f880f77948a3f8eaf0f4f734b08c877509df89
Connor McCoy <cmccoy@fhcrc.org>
2012-11-28 13:50:45 -0800

    support specifying deenurp

--- tests/hrefpkg-build/run.sh
+++ tests/hrefpkg-build/run.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 BASE=../rdp_10_30_named1200bp_subset
 mkdir -p hrefpkg
-deenurp hrefpkg-build --index-rank=family $BASE.fasta $BASE.seqinfo.csv $BASE.taxonomy.csv --output-dir hrefpkg
+DEENURP=${DEENURP-../../deenurp.py}
+$DEENURP hrefpkg-build --index-rank=family $BASE.fasta $BASE.seqinfo.csv $BASE.taxonomy.csv --output-dir hrefpkg --threads 6
--- tests/search-select/run.sh
+++ tests/search-select/run.sh
@@ -1,7 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 set -u
+set -e
 
 RDP_BASE=../rdp_10_30_named1200bp_subset
+DEENURP=${DEENURP-../../deenurp.py}
 
-deenurp search-sequences ground-reads.fa search.db $RDP_BASE.fasta $RDP_BASE.seqinfo.csv --group-field=tax_id
-deenurp select-references search.db refs.fasta --seqinfo-out refs.seqinfo.csv --output-meta refs.meta.csv --mpi-args '-np 2'
+$DEENURP search-sequences ground-reads.fa search.db $RDP_BASE.fasta $RDP_BASE.seqinfo.csv --group-field=tax_id
+$DEENURP select-references search.db refs.fasta --seqinfo-out refs.seqinfo.csv --output-meta refs.meta.csv --mpi-args '-np 2'
