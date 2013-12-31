d6735ae7cd59f726a9acba052f231e579deac9ac
Noah Hoffman <noah.hoffman@gmail.com>
2013-12-30 17:01:06 -0800

    - add top-level run script for tests - save test output in output directories (to more easily ignore them)

--- tests/filter-outliers/run.sh
+++ tests/filter-outliers/run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+set -e
+
+rm -rf output
+mkdir output
+
 BASE=../rdp_10_30_named1200bp_subset
 DEENURP=${DEENURP-../../deenurp.py}
-$DEENURP filter-outliers $BASE.fasta $BASE.seqinfo.csv $BASE.taxonomy.csv filtered.fasta --filtered-seqinfo filtered.seqinfo.csv
+$DEENURP filter-outliers $BASE.fasta $BASE.seqinfo.csv $BASE.taxonomy.csv output/filtered.fasta --filtered-seqinfo output/filtered.seqinfo.csv
