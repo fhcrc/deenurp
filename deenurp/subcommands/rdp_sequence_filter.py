462bbf14575f713954f523d29a8364820b0a3152
Noah Hoffman <noah.hoffman@gmail.com>
2014-10-10 15:12:27 -0700

    pep8

--- deenurp/subcommands/rdp_sequence_filter.py
+++ deenurp/subcommands/rdp_sequence_filter.py
@@ -9,9 +9,11 @@ from Bio import SeqIO
 
 from deenurp.util import Counter, file_opener
 
+
 def count_ambiguous(seq):
     s = frozenset('ACGT')
     return sum(i not in s for i in seq)
+
 
 def build_parser(p):
     p.add_argument('fasta_file', help="""sequence file""", type=file_opener('r'))
@@ -21,10 +23,11 @@ def build_parser(p):
 
     flt = p.add_argument_group('Filtering options')
     flt.add_argument('-a', '--prop-ambig-cutoff', default=0.01, type=float,
-            help="""Maximum proportion of characters in sequence which may be
-            ambiguous [default: %(default).2f]""")
+                     help="""Maximum proportion of characters in sequence which may be
+                     ambiguous [default: %(default).2f]""")
     flt.add_argument('-l', '--min-length', type=int, help="""Minimum sequence
             length [default: %(default)d]""", default=1200)
+
 
 def action(a):
     """
@@ -34,14 +37,15 @@ def action(a):
         sequences = Counter(SeqIO.parse(fasta_fp, 'fasta'))
         reader = csv.DictReader(seqinfo_fp)
         with open(a.named_base + '.fasta', 'w') as named_fa_fp, \
-             open(a.named_base + '.seq_info.csv', 'w') as named_si_fp, \
-             open(a.unnamed_base + '.fasta', 'w') as unnamed_fa_fp, \
-             open(a.unnamed_base + '.seq_info.csv', 'w') as unnamed_si_fp:
-            unnamed_writer = csv.DictWriter(unnamed_si_fp, reader.fieldnames,
-                    quoting=csv.QUOTE_NONNUMERIC)
+            open(a.named_base + '.seq_info.csv', 'w') as named_si_fp, \
+            open(a.unnamed_base + '.fasta', 'w') as unnamed_fa_fp, \
+            open(a.unnamed_base + '.seq_info.csv', 'w') as unnamed_si_fp:
+
+            unnamed_writer = csv.DictWriter(
+                unnamed_si_fp, reader.fieldnames, quoting=csv.QUOTE_NONNUMERIC)
             unnamed_writer.writeheader()
-            named_writer = csv.DictWriter(named_si_fp, reader.fieldnames,
-                    quoting=csv.QUOTE_NONNUMERIC)
+            named_writer = csv.DictWriter(
+                named_si_fp, reader.fieldnames, quoting=csv.QUOTE_NONNUMERIC)
             named_writer.writeheader()
 
             accepted = 0
@@ -66,4 +70,4 @@ def action(a):
                 SeqIO.write([sequence], fa_fp, 'fasta')
 
             logging.info("%d/%d passed (%.2f%%)", accepted, accepted+rejected,
-                    accepted/float(accepted+rejected) * 100.0)
+                         accepted/float(accepted+rejected) * 100.0)
