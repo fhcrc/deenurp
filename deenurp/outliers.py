151b3ccf9d7578a07fbb205f4c52cbedd07d2947
Noah Hoffman <noah.hoffman@gmail.com>
2014-12-09 10:28:01 -0800

    handle nan in distance matrix when identifying outliers

--- deenurp/outliers.py
+++ deenurp/outliers.py
@@ -7,7 +7,7 @@ import logging
 import subprocess
 import tempfile
 
-import numpy
+import numpy as np
 
 log = logging.getLogger(__name__)
 
@@ -19,7 +19,7 @@ def read_dists(fobj):
     """
 
     N = int(fobj.readline())
-    distmat = numpy.repeat(-1 * numpy.inf, N ** 2)
+    distmat = np.repeat(-1 * np.inf, N ** 2)
     distmat.shape = (N, N)
 
     taxa = []
@@ -67,15 +67,18 @@ def outliers(distmat, cutoff, min_size=3):
     """
 
     if distmat.shape[0] < min_size:
-        medoid = numpy.nan
-        dists = numpy.repeat(numpy.nan, distmat.shape[0])
-        to_prune = numpy.repeat(False, distmat.shape[0])
+        medoid = np.nan
+        dists = np.repeat(np.nan, distmat.shape[0])
+        to_prune = np.repeat(False, distmat.shape[0])
     else:
+        # use a masked array in case there are any nan
+        ma = np.ma.masked_array(distmat, np.isnan(distmat))
+
         # index of most central element.
-        medoid = numpy.argmin(numpy.median(distmat, 0))
+        medoid = np.argmin(np.median(ma, 0))
 
         # distance from each element to most central element
-        dists = distmat[medoid, :]
+        dists = ma[medoid, :]
         to_prune = dists > cutoff
 
     return medoid, dists, to_prune
