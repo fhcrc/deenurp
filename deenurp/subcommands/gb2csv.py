ebb609eb65d3262e876c4a03cce6c27d1d39829f
Chris Rosenthal <crosenth@uw.edu>
2015-07-21 12:50:23 -0700

    extending Bio.SeqRecord.SeqRecord and Bio.SeqFeature.Reference

--- deenurp/subcommands/gb2csv.py
+++ deenurp/subcommands/gb2csv.py
@@ -13,10 +13,11 @@ References
 """
 
 import csv
+import itertools
 import re
 import sys
 
-from Bio import SeqIO
+from Bio import SeqIO, SeqRecord, SeqFeature
 
 from deenurp.util import (accession_version_of_genbank,
                           tax_of_genbank, Counter, file_opener)
@@ -37,45 +38,95 @@ def build_parser(parser):
                         help="""output path [default: stdout]""")
 
 
+class SeqRec(SeqRecord.SeqRecord):
+    rec_names = ['version', 'accession', 'id', 'name', 'description',
+                 'gi', 'tax_id', 'date', 'source', 'keywords', 'organism']
+
+    def rec_dict(self):
+        return dict((a, self.__getattribute__(a)) for a in SeqRec.rec_names)
+
+    def __hash__(self):
+        """
+        Unique SeqRec ID
+
+        Combo of id, version and sequence should define uniqueness
+        """
+        return hash((self.id, self.version, str(self.seq)))
+
+    def hash(self):
+        if not hasattr(self, 'hash_id'):
+            self.hash_id = self.__hash__()
+        return self.hash_id
+
+    def references(self):
+        if 'references' in self.annotations:
+            refs = self.annotations['references']
+            return set(convert_record(ref, Ref) for ref in refs)
+        else:
+            return None
+
+    def setAttributes(self):
+        self.accession, self.version = accession_version_of_genbank(self)
+        self.date = self.annotations['date']
+        self.gi = self.annotations.get('gi', '')
+        self.keywords = ';'.join(self.annotations.get('keywords', []))
+        self.organism = self.annotations['organism']
+        self.source = self.annotations['source']
+        self.tax_id = tax_of_genbank(self)
+        return self
+
+
+class Ref(SeqFeature.Reference):
+    ref_names = ['version', 'title', 'authors', 'comment',
+                 'consrtm', 'journal', 'medline_id', 'pubmed_id']
+
+    def __hash__(self):
+        """
+        Unique Ref ID
+
+        Combo of all ref_names attributes
+        """
+        return hash([self.__getattribute__(a) for a in SeqRec.ref_names])
+
+    def hash(self):
+        if not hasattr(self, 'hash_id'):
+            self.hash_id = self.__hash__()
+        return self.hash_id
+
+    def ref_dict(self):
+        return dict((a, self.__getattribute__(a)) for a in SeqRec.ref_names)
+
+    def setAttributes(self, record_id):
+        title = self.title
+        self.title = title if re.search('\w', title) else ''
+        self.id = record_id
+        return self
+
+
+def convert_record(record, cls):
+    record.__class__ = cls
+    return record.setAttributes()
+
+
 def action(args):
     records = SeqIO.parse(args.infile, format='gb')
+    records = (convert_record(record, SeqRec) for record in records)
+    records = set(records)
     records = Counter(records, prefix='Record ')
 
-    fieldnames = ['version', 'accession', 'id', 'name', 'description',
-                  'gi', 'tax_id', 'date', 'source', 'keywords', 'organism']
-
-    out = csv.DictWriter(args.out, fieldnames=fieldnames)
+    out = csv.DictWriter(args.out, fieldnames=SeqRec.re_names)
     out.writeheader()
 
     if args.references_out:
-        refs_fieldnames = ['version', 'title', 'authors', 'comment',
-                           'consrtm', 'journal', 'medline_id', 'pubmed_id']
-        out_refs = csv.DictWriter(args.references_out,
-                                  fieldnames=refs_fieldnames)
+        fieldnames = SeqRec.rec_names
+        out_refs = csv.DictWriter(args.references_out, fieldnames=fieldnames)
         out_refs.writeheader()
 
-    for record in records:
-        accession, version = accession_version_of_genbank(record)
-        annotations = record.annotations
-        keywords = ';'.join(record.annotations.get('keywords', []))
-        out.writerow(dict(accession=accession,
-                          date=annotations['date'],
-                          description=record.description,
-                          gi=annotations.get('gi', ''),
-                          id=record.id,
-                          keywords=keywords,
-                          name=record.name,
-                          organism=annotations['organism'],
-                          source=annotations['source'],
-                          tax_id=tax_of_genbank(record),
-                          version=version))
+    records = sorted(records, key=records.version)
+    records = itertools.groupby(records, by=records.version)
 
-        if args.references_out and 'references' in annotations:
-            # dedup references per record
-            rows = set()
-            for ref in annotations['references']:
-                title = ref.title if re.search('\w', ref.title) else ''
-                rows.add((version, title, ref.authors, ref.comment,
-                          ref.consrtm, ref.journal,
-                          ref.medline_id, ref.pubmed_id))
-            out_refs.writerows(dict(zip(refs_fieldnames, row)) for row in rows)
+    for _, records in records:
+        out.writerows(r.rec_dict for r in records)
+
+        if args.references_out:
+            out_refs.writerows(ref.ref_dict for rec in records for ref in rec)
