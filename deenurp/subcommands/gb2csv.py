fb35d8adf430a2b19c5d9ba38506ed3d6fe30e57
Chris Rosenthal <crosenth@uw.edu>
2015-07-24 11:43:49 -0700

    updating to work with new gb2fa updates

--- deenurp/subcommands/gb2csv.py
+++ deenurp/subcommands/gb2csv.py
@@ -7,13 +7,14 @@ Genbank
 [id, name, description, version, accession, gi, tax_id,
  comment, date, source, keywords, organism]
 
-References
+Seq_References
 [version, title, authors, comment,
  consrtm, journal, medline_id, pubmed_id]
 """
 
 import csv
 import itertools
+import operator
 import re
 import sys
 
@@ -38,30 +39,39 @@ def build_parser(parser):
                         help="""output path [default: stdout]""")
 
 
-class SeqRec(SeqRecord.SeqRecord):
-    rec_names = ['version', 'accession', 'id', 'name', 'description',
-                 'gi', 'tax_id', 'date', 'source', 'keywords', 'organism']
+class Seq_Info(SeqRecord.SeqRecord):
+    attrs = ['version', 'accession', 'id', 'name', 'description',
+             'gi', 'tax_id', 'date', 'source', 'keywords', 'organism']
 
     def rec_dict(self):
-        return dict((a, self.__getattribute__(a)) for a in SeqRec.rec_names)
+        return dict((a, self.__getattribute__(a)) for a in Seq_Info.attrs)
+
+    def __eq__(self, other):
+        return isinstance(other, Seq_Ref) and self.hash() == other.hash()
 
     def __hash__(self):
         """
-        Unique SeqRec ID
+        Unique Seq_Info ID
 
-        Combo of id, version and sequence should define uniqueness
+        Uniqueness limited to id, version and sequence
         """
-        return hash((self.id, self.version, str(self.seq)))
-
-    def hash(self):
         if not hasattr(self, 'hash_id'):
-            self.hash_id = self.__hash__()
+            self.hash_id = hash((self.id, self.version))
         return self.hash_id
 
+    def hash(self):
+        return self.__hash__()
+
+    def has_references(self):
+        return 'references' in self.annotations
+
     def references(self):
-        if 'references' in self.annotations:
+        """
+        Might want to cache this in a variable if referencing more than once
+        """
+        if self.has_references():
             refs = self.annotations['references']
-            return set(convert_record(ref, Ref) for ref in refs)
+            return [convert_record(ref, Seq_Ref, self.version) for ref in refs]
         else:
             return None
 
@@ -76,57 +86,63 @@ class SeqRec(SeqRecord.SeqRecord):
         return self
 
 
-class Ref(SeqFeature.Reference):
-    ref_names = ['version', 'title', 'authors', 'comment',
-                 'consrtm', 'journal', 'medline_id', 'pubmed_id']
+class Seq_Ref(SeqFeature.Reference):
+    attrs = ['version', 'title', 'authors', 'comment',
+             'consrtm', 'journal', 'medline_id', 'pubmed_id']
 
     def __hash__(self):
         """
-        Unique Ref ID
+        Unique Seq_Ref ID
 
         Combo of all ref_names attributes
         """
-        return hash([self.__getattribute__(a) for a in SeqRec.ref_names])
-
-    def hash(self):
         if not hasattr(self, 'hash_id'):
-            self.hash_id = self.__hash__()
+            attributes = tuple(self.__getattribute__(a) for a in Seq_Ref.attrs)
+            self.hash_id = hash(attributes)
         return self.hash_id
 
-    def ref_dict(self):
-        return dict((a, self.__getattribute__(a)) for a in SeqRec.ref_names)
+    def __eq__(self, other):
+        return isinstance(other, Seq_Ref) and self.hash() == other.hash()
 
-    def setAttributes(self, record_id):
+    def hash(self):
+        return self.__hash__()
+
+    def ref_dict(self):
+        return dict((a, self.__getattribute__(a)) for a in Seq_Ref.attrs)
+
+    def setAttributes(self, version):
         title = self.title
         self.title = title if re.search('\w', title) else ''
-        self.id = record_id
+        self.version = version
         return self
 
 
-def convert_record(record, cls):
+def convert_record(record, cls, *attrs):
     record.__class__ = cls
-    return record.setAttributes()
+    return record.setAttributes(*attrs)
 
 
 def action(args):
     records = SeqIO.parse(args.infile, format='gb')
-    records = (convert_record(record, SeqRec) for record in records)
-    records = set(records)
+    records = (convert_record(record, Seq_Info) for record in records)
     records = Counter(records, prefix='Record ')
 
-    out = csv.DictWriter(args.out, fieldnames=SeqRec.re_names)
+    out = csv.DictWriter(args.out, fieldnames=Seq_Info.attrs)
     out.writeheader()
 
     if args.references_out:
-        fieldnames = SeqRec.rec_names
+        fieldnames = Seq_Ref.attrs
         out_refs = csv.DictWriter(args.references_out, fieldnames=fieldnames)
         out_refs.writeheader()
 
-    records = sorted(records, key=records.version)
-    records = itertools.groupby(records, by=records.version)
+    records = sorted(records, key=operator.attrgetter('version'))
+    records = itertools.groupby(records, key=operator.attrgetter('version'))
+    records = (list(recs) for ver, recs in records)
 
-    for _, records in records:
-        out.writerows(r.rec_dict for r in records)
-
+    for recs in records:
         if args.references_out:
-            out_refs.writerows(ref.ref_dict for rec in records for ref in rec)
+            # write reference file before deduplicating records
+            has_refs = [rec for rec in recs if rec.has_references()]
+            refs = set([ref for rec in has_refs for ref in rec.references()])
+            out_refs.writerows(ref.ref_dict() for ref in refs)
+        out.writerows(rec.rec_dict() for rec in set(recs))
