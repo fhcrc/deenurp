bbe79c238ea48ed12f0a850880b5f0090447b7fa
Chris Rosenthal <crosenth@uw.edu>
2015-08-19 16:55:47 -0700

    use util.version()

--- deenurp/__init__.py
+++ deenurp/__init__.py
@@ -1,9 +1,3 @@
-import os
-import sys
+import util
 
-try:
-    with open(os.path.join(os.path.dirname(__file__), 'data', 'ver')) as v:
-        __version__ = v.read().strip().lstrip('v')
-except Exception, e:
-    sys.stderr.write(e)
-    __version__ = ''
+__version__ = util.version()
