57b79d5691e0a20787c5334a17e87ef6fb647a96
Chris Rosenthal <crosenth@uw.edu>
2015-08-24 09:58:32 -0700

    deenurp may not be installed so need to try except that.  Added a few comments to version function as well.

--- deenurp/util.py
+++ deenurp/util.py
@@ -242,8 +242,9 @@ def require_executable(executable_name):
 def version():
     """
     Depending on if git exists and at what version we can try these commands
-    to get the git version from the git tags.  If all else return the install
-    version or 0.0.0
+    to get the git version from the git tags.  Second, if the package has been
+    installed return the installed version.  Finally, if nothing else,
+    return 0.0.0
     """
 
     install_dir = os.path.dirname(__file__)
@@ -251,13 +252,31 @@ def version():
                 ['git', 'describe', '--tags'])  # < 1.8.5
     devnull = open(os.devnull, 'w')
 
+    """
+    try the two git commands above ^
+    git versions are organized as
+    [tag]-[number of commits over tag]-[commit id]
+    ex - v0.1.2-38-g6a8e5e3
+    """
     for cmd in git_cmds:
         try:
-            logging.info(' '.join(cmd))
+            logging.debug(' '.join(cmd))
+            git_re = '(?P<tag>[\d.]*)-?(?P<commit>[\d.]*)-?.*'
             git_ver = subprocess.check_output(cmd, stderr=devnull)
-            tag_ver = re.search('v([\d.]*-[\d.]*)(-.*)?', git_ver).group(1)
-            return '.dev'.join(tag_ver.split('-'))
+            git_search = re.search(git_re, git_ver)
+            if git_search.group('commit') == '':
+                return git_search.group('tag')
+            else:
+                return '{tag}.dev{commit} '.format(**git_search.groupdict())
         except Exception as e:
-            logging.warn(e)
+            logging.warn('{} {}'.format(type(e), e.message))
 
-    return pkg_resources.require("deenurp")[0].version
+    try:
+        """
+        return version that was installed if available
+        """
+        return pkg_resources.require("csvpandas")[0].version
+    except pkg_resources.DistributionNotFound as e:
+        logging.warn(e)
+
+    return '0.0.0'
