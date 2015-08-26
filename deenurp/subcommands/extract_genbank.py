108a700d97d8f8f3588199a0d87452ec860e59a1
Chris Rosenthal <crosenth@uw.edu>
2015-08-26 13:30:43 -0700

    adding output for fasta file

--- deenurp/subcommands/extract_genbank.py
+++ deenurp/subcommands/extract_genbank.py
@@ -17,25 +17,33 @@ import itertools
 import operator
 import sys
 
+from Bio import SeqIO
 from deenurp import util, seq_info
 
 
 def build_parser(parser):
+    # input
     parser.add_argument('infile',
                         default=sys.stdin,
                         nargs='?',
                         type=util.file_opener(mode='r'),
                         help="""path to genbank file [default: stdin]""")
 
+    # outputs
+    parser.add_argument('fasta_out',
+                        type=util.file_opener(mode='w'),
+                        help='output fasta')
+    parser.add_argument('seqinfo_out',
+                        metavar='CSV',
+                        type=util.file_opener(mode='w'),
+                        help='output seq_info')
     parser.add_argument('--references-out',
                         type=util.file_opener(mode='w'),
                         help="""output references""")
+
+    # optionals
     parser.add_argument('--database', help="""Path to taxonomy database
                                               for validating tax_ids""")
-    parser.add_argument('--out',
-                        default=sys.stdout,
-                        type=util.file_opener(mode='w'),
-                        help="""output path [default: stdout]""")
 
 
 def action(args):
@@ -45,8 +53,9 @@ def action(args):
     records = seq_info.parse(args.infile, format='gb')
     records = util.Counter(records, prefix='Record ')
 
-    out = csv.DictWriter(args.out, fieldnames=seq_info.info_fieldnames)
-    out.writeheader()
+    seqinfo_out = csv.DictWriter(args.seqinfo_out,
+                                 fieldnames=seq_info.info_fieldnames)
+    seqinfo_out.writeheader()
 
     if args.references_out:
         fieldnames = seq_info.ref_fieldnames
@@ -58,10 +67,16 @@ def action(args):
     records = itertools.groupby(records, key=operator.attrgetter('version'))
     records = (list(recs) for ver, recs in records)
 
+    fa = []
+
     for recs in records:
         # write reference file before taking `set' of records
         if args.references_out:
             has_refs = [rec for rec in recs if rec.has_references()]
             refs = set([ref for rec in has_refs for ref in rec.references()])
             out_refs.writerows(ref.ref_dict() for ref in refs)
-        out.writerows(rec.record_dict() for rec in set(recs))
+        recs = set(recs)
+        seqinfo_out.writerows(rec.record_dict() for rec in recs)
+        fa.extend(recs)
+
+    SeqIO.write(fa, args.fasta_out, 'fasta')
