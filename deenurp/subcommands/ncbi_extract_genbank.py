f1210a224db4b3e4ed3f9623fbb9c5d0e923267f
Chris Rosenthal <crosenth@uw.edu>
2015-09-22 12:00:42 -0700

    removed license text at top until we add it everywhere

--- deenurp/subcommands/ncbi_extract_genbank.py
+++ deenurp/subcommands/ncbi_extract_genbank.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-
-#    Entrez.py is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    Entrez.py is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with Entrez.py.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 NCBI Entrez tool for downloading nucleotide Genbank records
 given list of accession numbers, GI numbers. Provides
