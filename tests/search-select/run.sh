4b77f8a330c3805a582c2aa1c2c34cc676ec42c9
Noah Hoffman <noah.hoffman@gmail.com>
2016-03-31 17:12:18 -0700

    less verbosely again

--- tests/search-select/run.sh
+++ tests/search-select/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 set -u
 set -e
 
@@ -8,5 +8,12 @@ DEENURP=${DEENURP-../../deenurp.py}
 rm -rf output
 mkdir -p output
 
-$DEENURP search_sequences ground-reads.fa output/search.db $RDP_BASE.fasta $RDP_BASE.seqinfo.csv --group-field=tax_id --blacklist=exclude_clusters.txt
-$DEENURP select_references -v output/search.db output/refs.fasta --seqinfo-out output/refs.seqinfo.csv --output-meta output/refs.meta.csv --min-mass-prop 0.01 --include-clusters include_clusters.txt
+$DEENURP search_sequences ground-reads.fa output/search.db $RDP_BASE.fasta $RDP_BASE.seqinfo.csv \
+	 --group-field=tax_id \
+	 --blacklist=exclude_clusters.txt
+
+$DEENURP select_references output/search.db output/refs.fasta \
+	 --seqinfo-out output/refs.seqinfo.csv \
+	 --output-meta output/refs.meta.csv \
+	 --min-mass-prop 0.01 \
+	 --include-clusters include_clusters.txt
