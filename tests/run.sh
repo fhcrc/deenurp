361cc5d76c984839aa8f7558c768420f75bcb2cd
Noah Hoffman <noah.hoffman@gmail.com>
2016-04-04 11:53:18 -0700

    run search-select only

--- tests/run.sh
+++ tests/run.sh
@@ -4,6 +4,9 @@ set -e
 
 vsearch --version
 
+(cd tests/search-select && ./run.sh)
+exit
+
 TESTS_DIR=$(dirname $BASH_SOURCE)
 
 for f in $(find $TESTS_DIR -wholename $TESTS_DIR/run.sh -prune -o -name run.sh -print); do
