ec5ae584e50b1078bb64c00d796fce79fa40d8f3
Chris Rosenthal <crosenth@uw.edu>
2016-06-14 10:48:52 -0700

    --seq-info-out was moved to derep-map-out and replaced with an actual seqinfo file

--- deenurp/subcommands/dereplicate_named.py
+++ deenurp/subcommands/dereplicate_named.py
@@ -3,6 +3,7 @@
 """
 
 import logging
+import sys
 
 import pandas as pd
 
@@ -18,19 +19,28 @@ def build_parser(parser):
                         help="""Taxonomy as taxtable; optional
                         if a grouping term is available in seq_info""")
 
-    parser.add_argument('--seq-info-out', type=util.file_opener('w'))
-    parser.add_argument('--seqs-out', type=util.file_opener('w'))
+    parser.add_argument('--seq-info-out',
+                        help='subset of original seq_info')
+    parser.add_argument('--derep-map-out',
+                        help=('mapping of input sequences to dereplicated '
+                              'representatives. `group` column corresponds to '
+                              'the field identified by --group-on.'))
+    parser.add_argument('--seqs-out',
+                        default=sys.stdout,
+                        type=util.file_opener('w'))
 
     parser.add_argument('-g', '--group-on', default='species',
-                        help="""Field in seq_info on which to group sequences""")
+                        help='Field in seq_info on which to group sequences')
     parser.add_argument('--id', default=1.0,
                         type=float, help="""Clustering identity between 0 and 1
                         [default: %(default).3f]""")
     parser.add_argument('-i', '--include', type=util.file_opener('r'),
-                        help="""Optional file containing list of group labels to include""")
-    parser.add_argument('--threads', help="""Number of threads to use
-        for clustering each group [default is one thread per available
-        CPU core]""")
+                        help=('Optional file containing list '
+                              'of group labels to include'))
+    parser.add_argument('--threads',
+                        help=('Number of threads to use for clustering each '
+                              'group [default is one thread per '
+                              'available CPU core]'))
 
 
 def mocked_cluster_output(seqnames):
@@ -42,7 +52,7 @@ def mocked_cluster_output(seqnames):
 
 def cluster(seqfile, seqnames, identity=1.0, prefix='cluster-', threads=None):
     with util.ntf(prefix=prefix, suffix='.fasta') as fa, \
-         util.ntf(prefix=prefix, suffix='.uc') as uc:
+            util.ntf(prefix=prefix, suffix='.uc') as uc:
         wrap.esl_sfetch(seqfile, seqnames, fa)
         fa.flush()
         uclust.cluster(fa.name,
@@ -88,9 +98,14 @@ def action(args):
 
     all_clusters = pd.concat(frames)
 
-    if args.seq_info_out:
+    if args.derep_map_out:
         all_clusters.columns = ['type', 'seqname', 'seed', 'group']
-        all_clusters.to_csv(args.seq_info_out, header=True, index=False)
+        all_clusters.to_csv(args.derep_map_out, header=True, index=False)
+
+    if args.seq_info_out:
+        seq_info = seq_info[seq_info['seqname'].isin(all_clusters['seed'])]
+        seq_info.to_csv(args.seq_info_out, index=False)
 
     if args.seqs_out:
-        wrap.esl_sfetch(args.seqs, all_clusters['seed'].unique(), args.seqs_out)
+        wrap.esl_sfetch(
+            args.seqs, all_clusters['seed'].unique(), args.seqs_out)
