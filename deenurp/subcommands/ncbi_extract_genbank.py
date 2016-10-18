5938f544e07bbc2f5e21703098410f8d77fcf97b
Chris Rosenthal <crosenth@uw.edu>
2016-10-18 14:17:41 -0700

    cleaned up write_or_append

--- deenurp/subcommands/ncbi_extract_genbank.py
+++ deenurp/subcommands/ncbi_extract_genbank.py
@@ -32,6 +32,8 @@ reference_columns = ['pubmed_id', 'medline_id', 'title',
 
 pubmed_columns = ['pubmed_id', 'version', 'accession']
 
+ACGT = frozenset('ACGT')
+
 
 def build_parser(parser):
     # ins
@@ -77,11 +79,6 @@ def build_parser(parser):
                         default=60000,
                         help="""after http exception time to
                                 wait before trying again [%(default)s]""")
-    parser.add_argument('--max-records',
-                        type=int,
-                        metavar='int',
-                        help="""limit number of records from
-                                esearch or ids file to N""")
 
     parser.add_argument('--all-versions',
                         action='store_true',
@@ -140,8 +137,7 @@ def accession_version_of_genbank(record):
 
 
 def count_ambiguous(seq):
-    s = frozenset('ACGT')
-    return sum(i not in s for i in seq)
+    return sum(i not in ACGT for i in seq)
 
 
 def latest_versions(ids):
@@ -149,8 +145,9 @@ def latest_versions(ids):
     http://www.ncbi.nlm.nih.gov/genbank/sequenceids/
     """
 
+    def versions(x):
+        return re.search('.+\.?', x).group()
     ordered = sorted(ids, reverse=True)
-    versions = lambda x: re.search('.+\.?', x).group()
     grouped = itertools.groupby(ordered, key=versions)
 
     for _, ids in grouped:
@@ -162,6 +159,8 @@ def is_type(record):
     Returns a boolean indicating whether a sequence is a member of a type
     strain, as indicated by the presence of the string '(T)' within the
     record description.
+
+    Note: NOT YET USED
     """
     type_keywords = ['(T)', 'ATCC', 'NCTC', 'NBRC', 'CCUG',
                      'DSM', 'JCM', 'NCDO', 'NCIB', 'CIP']
@@ -226,71 +225,32 @@ def parse_references(record):
     return references
 
 
-def write_or_append(fasta_path, info_path, pubmed_ids_path, references_path):
-    if os.path.isfile(fasta_path):
-        log.info('{} exists, appending new results'.format(fasta_path))
-        fasta_out = open(fasta_path, 'a')
+def write_or_append(path, mode='w', bufsize=0, fieldnames=None):
+    if os.path.isfile(path):
+        log.info(path + ' exists, appending results')
+        mode = 'a'
+
+    if fieldnames is None:
+        out = open(path, mode, bufsize)
     else:
-        fasta_out = open(fasta_path, 'w')
+        if mode == 'a':
+            with open(path, 'r') as path_in:
+                in_fieldnames = csv.DictReader(path_in).fieldnames
 
-    if os.path.isfile(info_path):
-        log.info('{} exists, appending new results'.format(info_path))
+            if not all(f in fieldnames for f in in_fieldnames):
+                msg = 'input columns {} != {}'
+                msg = msg.format(fieldnames, in_fieldnames)
+                raise ValueError(msg)
 
-        # get column order
-        with open(info_path) as info_in:
-            fieldnames = csv.DictReader(info_in).fieldnames
-        if set(fieldnames) != set(seq_info_columns):
-            msg = 'input columns {} != {}'.format(fieldnames, seq_info_columns)
-            raise ValueError(msg)
+            fieldnames = in_fieldnames  # preserve order
 
-        info_out = csv.DictWriter(
-            open(info_path, 'a'), fieldnames=fieldnames, extrasaction='ignore')
-    else:
-        info_out = csv.DictWriter(
-            open(info_path, 'w'),
-            fieldnames=seq_info_columns,
-            extrasaction='ignore')
-        info_out.writeheader()
+        out = csv.DictWriter(
+            open(path, mode, bufsize), fieldnames, extrasaction='ignore')
 
-    if pubmed_ids_path is not None and os.path.isfile(pubmed_ids_path):
-        log.info('{} exists, appending new results'.format(pubmed_ids_path))
+        if mode == 'w':
+            out.writeheader()
 
-        # get column order
-        with open(pubmed_ids_path) as pubmed_ids_in:
-            fieldnames = csv.DictReader(pubmed_ids_in).fieldnames
-        if set(fieldnames) != set(pubmed_columns):
-            msg = 'input columns {} != {}'.format(fieldnames, pubmed_columns)
-            raise ValueError(msg)
-
-        pubmed_ids_out = csv.DictWriter(
-            open(pubmed_ids_path, 'a'), fieldnames=fieldnames)
-    elif pubmed_ids_path is not None:
-        pubmed_ids_out = csv.DictWriter(
-            open(pubmed_ids_path, 'w'), fieldnames=pubmed_columns)
-        pubmed_ids_out.writeheader()
-    else:
-        pubmed_ids_out = None
-
-    if references_path is not None and os.path.isfile(references_path):
-        log.info('{} exists, appending new results'.format(references_path))
-
-        # get column order
-        with open(references_path) as references_in:
-            fieldnames = csv.DictReader(references_in).fieldnames
-        if set(fieldnames) != set(reference_columns):
-            msg = 'input columns {} != {}'
-            raise ValueError(msg.format(fieldnames, reference_columns))
-
-        references_out = csv.DictWriter(
-            open(references_path, 'a'), fieldnames=fieldnames)
-    elif references_path is not None:
-        references_out = csv.DictWriter(
-            open(references_path, 'w'), fieldnames=reference_columns)
-        references_out.writeheader()
-    else:
-        references_out = None
-
-    return fasta_out, info_out, pubmed_ids_out, references_out
+    return out
 
 
 def parse_type_strain_set(types_file):
@@ -299,7 +259,6 @@ def parse_type_strain_set(types_file):
         types = set(t for t in types if t)
     else:
         types = set()
-
     return types
 
 
@@ -318,15 +277,30 @@ def expand_accession_ranges(accessions):
 
 
 def action(args):
+    # setup output files
+    fasta_out = write_or_append(args.fasta_out)
+    info_out = write_or_append(args.info_out, fieldnames=seq_info_columns)
+    if args.pubmed_ids:
+        pubmed_ids_out = write_or_append(
+            args.pubmed_ids, fieldnames=pubmed_columns)
+    else:
+        pubmed_ids_out = None
+    if args.references:
+        references_out = write_or_append(
+            args.references, fieldnames=reference_columns)
+    else:
+        references_out = None
+
+    # required by entrez
     entrez.set_email(args.email)
 
-    chunksize = min(entrez.RETMAX, args.chunksize)  # 10k is ncbi max right now
+    # 10k is entrez max right now
+    chunksize = min(entrez.RETMAX, args.chunksize)
 
     ids = (i for i in args.ids if not i.startswith('#'))  # ignore comments
     ids = (i.strip() for i in ids)  # remove newlines
     ids = (i for i in ids if i)  # ignore blank lines
     ids = expand_accession_ranges(ids)  # id ranges
-    ids = itertools.islice(ids, args.max_records)
 
     # take latest version accession
     if not args.all_versions:
@@ -351,10 +325,7 @@ def action(args):
 
     pool = multiprocessing.Pool(processes=args.threads)
     records = pool.imap_unordered(func, ids)
-    # records = itertools.imap(func, ids)
-
-    fasta_out, info_out, pubmed_ids_out, references_out = write_or_append(
-        args.fasta_out, args.info_out, args.pubmed_ids, args.references)
+    # records = itertools.imap(func, ids)  # debugging
 
     types = parse_type_strain_set(args.type_strains)
 
