96176a7628c49bb3ec7fa65e128867117900127a
Chris Rosenthal <crosenth@uw.edu>
2017-10-10 11:02:17 -0700

    remove pyc files before building docker image

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -9,6 +9,9 @@ if ! git diff-index --quiet HEAD; then
     exit 1
 fi
 
+# remove any lingering pyc files
+find . -name '*.pyc' -exec rm {} \;
+
 # docker tag should reflect current version number
 tag=deenurp:$(git describe --tags --dirty)
 docker build -t $tag .
