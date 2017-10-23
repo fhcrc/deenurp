162f74d88d5ffc6f31846d15d11a6da528c961e2
Noah Hoffman <noah.hoffman@gmail.com>
2017-10-23 15:15:10 -0700

    ignore *.pyc in with .dockerignore

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -9,9 +9,6 @@ if ! git diff-index --quiet HEAD; then
     exit 1
 fi
 
-# remove any lingering pyc files
-find . -name '*.pyc' -exec rm {} \;
-
 # docker tag should reflect current version number
 tag=deenurp:$(git describe --tags --dirty)
 docker build -t $tag .
